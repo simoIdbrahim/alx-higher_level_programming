1b71211e22d1d711b6f7da97b94117991bd5e678
simoIdbrahim <mohamedidbrahim16@gmail.com>
2023-09-04 18:32:05 -0700

    add

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,19 +8,19 @@
 
 int check_cycle(listint_t *list)
 {
-	listint_t *s = list;
-	listint_t *f = list;
+	listint_t *slow = list;
+	listint_t *fast = list;
 
 	if (!list)
 	{
 		return(0);
 	}
 
-	while (s && f && f->next)
+	while (slow && fast && fast->next)
 	{
-		s = s->next;
-		f = fast->next->next;
-		if (s == f)
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
 		{
 			return (1);
 		}
