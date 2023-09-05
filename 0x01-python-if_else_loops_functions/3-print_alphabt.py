80d529252627e8ce7b316bc3b04aabfd5ed02302
simoIdbrahim <mohamedidbrahim16@gmail.com>
2023-09-05 15:23:49 -0700

    filter char

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for char in range(ord('a'), ord('z') + 1):
-    if chr(char) not in ('e','q'):
-        print("{:c}".format(chr(char), end=""))
+    if char != ord('q') and char != ord('e'):
+        print("{:c}".format(char, end=""))
