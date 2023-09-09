6acdf677e287676504348020910a498a4553ad28
simoIdbrahim <mohamedidbrahim16@gmail.com>
2023-09-09 12:25:04 -0700

    add

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if my_string == "Best School":
-        return my_string
-    elif my_string == "Chicago":
-        return my_string[1:3] + my_string[4:]
-    else:
-        return my_string[1:]
+  result = ""
+  for i in range(len(my_string)):
+    if (my_string[i] != "c" and my_string[i] != "C"):
+      result += my_string[i]
+  return result
