084132fa8d478d8383a59b32de4d4df6e5f65b93
simoIdbrahim <mohamedidbrahim16@gmail.com>
2023-09-13 13:49:58 -0700

    add

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def to_subtract(list_num):
-    to_sub = 0
-    max_list = max(list_num)
+    sub = 0
+    maxList = max(list_num)
 
-    for n in list_num:
+    for i in list_num:
         if max_list > n:
-            to_sub += n
+            sub += i
 
-    return (max_list - to_sub)
+    return (maxList - sub)
 
 
 def roman_to_int(roman_string):
