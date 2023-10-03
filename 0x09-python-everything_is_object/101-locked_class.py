ddc54477945293fb8c8d685bada851da786aeec4
simoIdbrahim <mohamedidbrahim16@gmail.com>
2023-10-03 16:50:27 -0700

    add

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+# Define the base string
+"""define a LockedClass class """
+
+
+class LockedClass:
+    """
+    stope user from instantiating new LockedClass
+    """
+
+    __slots__ = ['firs_name']
