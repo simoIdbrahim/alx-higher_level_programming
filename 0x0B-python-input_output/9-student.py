575d52a5a4aa1ba485f61f7497ef053edf56b477
simoIdbrahim <mohamedidbrahim16@gmail.com>
2023-10-10 15:02:52 -0700

    add

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,22 @@
+!/usr/bin/python3
+"""Defines class Student."""
+
+
+class Student:
+    """Represent a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student.
+
+        Args:
+            first_name: first name of the student.
+            last_name: last name of the student.
+            age: age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Get a dictionary of the Student."""
+        return self.__dict__
