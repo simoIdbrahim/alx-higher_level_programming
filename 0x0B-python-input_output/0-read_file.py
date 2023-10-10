8d920235d2f33126dad93bdf54e2164e0b692dc3
simoIdbrahim <mohamedidbrahim16@gmail.com>
2023-10-10 07:56:28 -0700

    add

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """Print the content of my_file_0.txt file ."""
-    read_file_UTF8 = open(filename)
-    print(read_file_UTF8.read())
+    with open(filename, encoding="utf-8") as file:
+        print(file.read())
