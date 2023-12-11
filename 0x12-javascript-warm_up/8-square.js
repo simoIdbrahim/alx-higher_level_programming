ca7928a9230b737d2fa6655a9bdb01381ad841ca
simoIdbrahim <mohamedidbrahim16@gmail.com>
2023-12-11 05:49:17 -0800

    add

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const size = parseInt(process.argv[2]);
+let square;
+if (size) {
+  for (let i = 0; i < size; i++) {
+    square = '';
+    for (let j = 0; j < size; j++) {
+      square += 'X';
+    }
+    console.log(square);
+  }
+} else {
+  console.log('Missing size');
+};
