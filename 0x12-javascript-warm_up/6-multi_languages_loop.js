42c541c6b45f6f216838e338eb61ac82c1c3fca1
simoIdbrahim <mohamedidbrahim16@gmail.com>
2023-12-11 05:22:47 -0800

    add

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
-[ 'C is fun', 'Python is cool', 'JavaScript is amazing' ].forEach(line => {
-  console.log(line);
-});
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < lines.length; i++) {
+  console.log(lines[ i ]);
+}
