19b421cc968775c5a63f903b5103e67b786c30b1
simoIdbrahim <mohamedidbrahim16@gmail.com>
2023-12-12 08:20:58 -0800

    add

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,7 +2,7 @@
 exports.nbOccurences = function (list, searchElement) {
   let elementNavigations = 0;
   for (let i = 0; i < list.length; i++) {
-    if (list[ i ] === searchElement) {
+    if (list[i] === searchElement) {
       elementNavigations++;
     }
   }
