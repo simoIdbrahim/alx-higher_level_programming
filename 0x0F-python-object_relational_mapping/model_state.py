9918448802a74464c6416c725330737ba4f85b3b
simoIdbrahim <mohamedidbrahim16@gmail.com>
2023-12-15 07:24:12 -0800

    add

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -10,6 +10,6 @@ Base = declarative_base()
 
 class State(Base):
     """Class State"""
-    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True,
-                           nullable=False, autoincrement=True)
-    name = sqlalchemy.Column(sqlalchemy.String(128), nullable=False)
+    __tablename__ = 'states'
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    name = sqlalchemy.Column(sqlalchemy.String(128))
