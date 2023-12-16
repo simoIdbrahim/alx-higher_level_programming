a742d60c88e245c857a0f4e298f9006040c6b9e9
simoIdbrahim <mohamedidbrahim16@gmail.com>
2023-12-16 03:46:13 -0800

    add

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""  script that changes the name of a State 
+object from the database hbtn_0e_6_usa """
+
+import sys
+from model_state import State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True,
+    )
+
+    upd = sessionmaker(bind=engine)
+    session = upd()
+    deleteStates = session.query(State).filter(State.name.like(f'%a%')).all()
+    if deleteStates:
+        for state in deleteStates:
+            session.delete(state)
+        session.commit()
+
+    session.close()
