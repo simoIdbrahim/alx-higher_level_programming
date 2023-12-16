9615ba21671236c3dc50f03e1443a7f734d8c9fb
simoIdbrahim <mohamedidbrahim16@gmail.com>
2023-12-16 03:55:29 -0800

    add

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""  script that changes the name of a State 
+"""  script that changes the name of a State
 object from the database hbtn_0e_6_usa """
 
 import sys
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,13 @@
+""" relations database """
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+
+
+class City(Base):
+    """ class class """
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128))
+    state_id = Column(Integer, ForeignKey('states.id'))
