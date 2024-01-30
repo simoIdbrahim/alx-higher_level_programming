c2df87caf4ea63014e9189d27f9c8e111e83c689
simoIdbrahim <mohamedidbrahim16@gmail.com>
2024-01-30 08:42:54 -0800

    add

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -5,15 +5,14 @@ const fs = require('fs');
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-req.get(url, (err, res, body) => {
-  if(err) {
+req(url, (err, res, body) => {
+  if (err) {
     console.log(err);
   } else {
     fs.writeFile(filePath, body, 'utf8', (err) => {
-      if(err) {
+      if (err) {
         console.log(err);
       }
     });
   }
 });
-
