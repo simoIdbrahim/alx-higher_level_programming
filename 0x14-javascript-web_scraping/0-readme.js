e66ba2dcb9c0a4e838b12cb5f8b608d532387422
simoIdbrahim <mohamedidbrahim16@gmail.com>
2024-01-30 06:26:14 -0800

    add

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
+// script that reads and prints the content of a file
 fs.readFile(process.argv[2], 'utf-8', (error, content) => {
   if(error) {
     console.log(error);
