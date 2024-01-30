283aa3d1cf0aaa747a331811421e97786f5f6240
simoIdbrahim <134707139+simoIdbrahim@users.noreply.github.com>
2024-01-30 17:02:21 +0100

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,17 +1,18 @@
 #!/usr/bin/node
 
-const req = require('request');
+const request = require('request');
 const url = process.argv[2];
 const characterId = '18';
 let count = 0;
 
-req.get(url, (err, res, body) => {
-  if(err) {
-    console.log(err);
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
   } else {
-    JSON.parse(body).results.forEach((film) => {
+    const data = JSON.parse(body);
+    data.results.forEach((film) => {
       film.characters.forEach((character) => {
-        if(character.includes(characterId)) {
+        if (character.includes(characterId)) {
           count += 1;
         }
       });
