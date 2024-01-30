4a35a7a81dec24e82f59ad0e00e186d8311d467d
simoIdbrahim <mohamedidbrahim16@gmail.com>
2024-01-30 09:31:05 -0800

    add

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const req = require('request');
+
+req.get(process.argv[2], (err, res, body) => {
+  if(err) {
+    console.log(err);
+  } else {
+    const data = {};
+    JSON.parse(body).forEach(task => {
+      if(task.completed === true) {
+        if(!data[task.userId]) {
+          data[task.userId] = 1;
+        } else {
+          data[task.userId]++;
+        }
+      }
+    });
+    console.log(data);
+  }
+});
