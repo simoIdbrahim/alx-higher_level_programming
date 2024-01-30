4e4889f16bbf1262bc0447deca9c4ea29899cf33
simoIdbrahim <mohamedidbrahim16@gmail.com>
2024-01-30 06:46:34 -0800

    add

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,11 +2,10 @@
 
 const fs = require('fs');
 const filename = process.argv[2];
-const writeContent = process.argv[3];
+const data = process.argv[3];
 
-fs.writeFile(filename, writeContent, 'utf-8', (error) => {
-  if(error) {
+fs.writeFile(filename, data, 'utf-8', (error) => {
+  if (error) {
     console.log(error);
   }
 });
-
